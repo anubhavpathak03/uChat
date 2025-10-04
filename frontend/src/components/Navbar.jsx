49807246d174e7bc63e8a625329fa54a4978d2e0
Anubhav Pathak <pathak.anubhavdtu@gmail.com>
2025-10-04 10:28:40 +0530

    update  chnages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { LogOut, MessageSquare, Settings, User } from 'lucide-react';
 const NavBar = () => {
   const {logout, authUser} = useAuthStore();
   return (
-    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+    <header className="bg-base  border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
       <div className='container mx-auto px-4 h-16'>
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
